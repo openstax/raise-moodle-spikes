b2c32e5d17f4f928203a3bbeb9330f342f7dcf91
mjoss17 <matthewjoss17@gmail.com>
2022-03-09 16:42:20 -0600

    content replacement filter added

---
+++ plugins/content/version.php
@@ -0,0 +1,5 @@
+<?php
+
+defined('MOODLE_INTERNAL') || die();
+$plugin->version   = 2022021400;        // The current plugin version (Date: YYYYMMDDXX)
+$plugin->component = 'filter_content'; // Full name of the plugin (used for diagnostics)
