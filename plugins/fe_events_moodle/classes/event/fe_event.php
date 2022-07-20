49848038e6e83009e015217ce3e9e850bb3e40fe
Prabhdip Gill <pg53@wn79c2tv3w.lan>
2022-07-20 00:27:51 -0700

    Add fe_event.php

---
+++ plugins/fe_events_moodle/classes/event/fe_event.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace local_fe_events_moodle\event;
+
+defined('MOODLE_INTERNAL') || die();
+
+class fe_event extends \core\event\base{
+
+
+    protected function init() {
+        $this->context = \context_system::instance();
+        $this->data['crud'] = 'c';
+        $this->data['edulevel'] = self::LEVEL_PARTICIPATING;
+
+
+
+    }
+
+    protected function validate_data() {
+        parent::validate_data();
+        // Make sure this class is never used without proper object details.
+        if (!$this->contextlevel === CONTEXT_MODULE) {
+            throw new \coding_exception('Context level must be CONTEXT_MODULE.');
+        }
+    }
+
+}
