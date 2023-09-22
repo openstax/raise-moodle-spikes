2cd5c5b2a50a830b40f7bbb2e032434763247f30
Ripal Nathuji <rnathuji@neoteklabs.com>
2023-09-22 15:05:07 -0500

    Implement local_persist_put

--- plugins/local_persist/classes/external/data.php
+++ plugins/local_persist/classes/external/data.php
@@ -22,10 +22,39 @@ class data extends external_api {
     }
 
     public static function put_data($courseid, $key, $value) {
+        global $USER, $DB;
+
         $params = self::validate_parameters(
             self::put_data_parameters(),
             ['courseid' => $courseid, 'key' => $key, 'value' => $value]
         );
+
+        $existingdata = $DB->get_record(
+            'local_persist_data',
+            ['user_id' => $USER->id, 'course_id' => $params['courseid'], 'data_key' => $params['key']],
+            'id,user_id,course_id,data_key',
+            IGNORE_MISSING
+        );
+
+        if ($existingdata) {
+            $existingdata->data_value = $params['value'];
+            $DB->update_record(
+                'local_persist_data',
+                $existingdata
+            );
+        } else {
+            $newdata = [
+                'user_id' => $USER->id,
+                'course_id' => $params['courseid'],
+                'data_key' => $params['key'],
+                'data_value' => $params['value']
+            ];
+
+            $DB->insert_record(
+                'local_persist_data',
+                $newdata
+            );
+        }
 
         return [
         ];
