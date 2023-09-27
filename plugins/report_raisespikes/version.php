591076315d6dc97fb940939f960277b529dfe9b1
Prabhdip Gill <pg53@rice.edu>
2023-09-27 08:35:03 -0700

    Fix docker-compose and remove strings

--- plugins/report_raisespikes/version.php
+++ plugins/report_raisespikes/version.php
@@ -3,5 +3,5 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2023092601;       // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2023092602;       // The current plugin version (Date: YYYYMMDDXX).
 $plugin->component = 'report_raisespikes'; // Full name of the plugin (used for diagnostics)
